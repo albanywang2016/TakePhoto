fbfbe4ba2cbacd9317c034d377e3261366e5ceb6
leiwang <albanywang2000@gmail.com>
2017-08-15 23:10:33 -0400

    take photo success, need to upload file to server.

--- app/src/main/java/japan/news/tech/takephoto/MainActivity.java
+++ app/src/main/java/japan/news/tech/takephoto/MainActivity.java
@@ -16,60 +16,57 @@ import android.widget.ImageView;
 import android.widget.Toast;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
 
 public class MainActivity extends AppCompatActivity {
 
     private static final int TAKE_PICTURE = 1;
     private Uri imageUri;
+    int TAKE_PHOTO_CODE = 0;
+    static int count = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final String dir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES) + "/picFolder/";
+        //final String dir = "http://japannews.tech/D9VZ68/";
+        File newdir = new File(dir);
+        newdir.mkdirs();
 
         Button btn = (Button) findViewById(R.id.btn_TakePhoto);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                takePhoto(view);
+                count++;
+                String file = dir+count+".jpg";
+                System.out.print("Take Photo" + file);
+                File newFile = new File(file);
+                try{
+                    newFile.createNewFile();
+                }catch (IOException e){
+                    e.printStackTrace();
+
+                }
+
+                Uri outputFileUri = Uri.fromFile(newFile);
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, outputFileUri);
+                startActivityForResult(intent, TAKE_PHOTO_CODE);
             }
         });
     }
 
-    public void takePhoto(View view) {
-        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        File photo = new File(Environment.getExternalStorageDirectory(),  "Pic1.jpg");
-        intent.putExtra(MediaStore.EXTRA_OUTPUT,
-                Uri.fromFile(photo));
-        imageUri = Uri.fromFile(photo);
-        startActivityForResult(intent, TAKE_PICTURE);
-    }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        switch (requestCode) {
-            case TAKE_PICTURE:
-                if (resultCode == Activity.RESULT_OK) {
-                    Uri selectedImage = imageUri;
-                    getContentResolver().notifyChange(selectedImage, null);
-                    ImageView imageView = (ImageView) findViewById(R.id.ImageView);
-                    ContentResolver cr = getContentResolver();
-                    Bitmap bitmap;
-                    try {
-                        bitmap = android.provider.MediaStore.Images.Media
-                                .getBitmap(cr, selectedImage);
-
-                        imageView.setImageBitmap(bitmap);
-                        Toast.makeText(this, selectedImage.toString(),
-                                Toast.LENGTH_LONG).show();
-                    } catch (Exception e) {
-                        Toast.makeText(this, "Failed to load", Toast.LENGTH_SHORT)
-                                .show();
-                        Log.e("Camera", e.toString());
-                    }
-                }
+        if(requestCode == TAKE_PHOTO_CODE && requestCode == RESULT_OK){
+            Log.d("Take Photo", "Pic saved");
         }
     }
+    
 
 }
